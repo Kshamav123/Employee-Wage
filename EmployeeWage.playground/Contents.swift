e7320c85c267d630ef70d0af6e8dd2a5684e3bfa
Kshamav123 <kshamavidya1999@gmail.com>
2021-10-04 11:36:00 +0530

    Added Part time

--- EmployeeWage.playground/Contents.swift
+++ EmployeeWage.playground/Contents.swift
@@ -1,17 +1,17 @@
 import UIKit
 
 /**
- To check if the employee is present or absent
+ To check if the employement is full time or part time 
  by genearting random number and also calculating daily wage
  of the employee
  */
 print("Welcome to Employee Wage computeation")
 
 let Is_Full_Time = 1
+let Is_Part_Time = 2
 let Wage_Per_Hr = 20
 
 var empHrs = 0
-var empWage = 0
 var empCheck = Int.random(in: 0..<5)
 if empCheck == Is_Full_Time
 {
@@ -19,10 +19,14 @@ if empCheck == Is_Full_Time
     empHrs=8
     
 }
-else
+else if empCheck == Is_Part_Time
 {
     print("Part time emplyement")
-    empHrs=0
+    empHrs=4
+}
+else
+{
+    empHrs = 0
 }
 var empDailyWage = Wage_Per_Hr * empHrs
 print("Employee daily wage: ")
