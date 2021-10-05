fcc738b19c3d0eab734b33a56907b2a767bfbeeb
Kshamav123 <kshamavidya1999@gmail.com>
2021-10-05 11:12:40 +0530

    Added multiple companys

--- EmployeeWage.playground/Contents.swift
+++ EmployeeWage.playground/Contents.swift
@@ -1,51 +1,53 @@
 import UIKit
 
 /**
- To check if the employement is full time or part time using case statement
- by genearting random number and also calculating wages for a month till
- a condition of 100 hours and 20 days are reached.
+ Computing employee wages for multiple companies
  */
 class EmployeeWage {
-    
-    var empDailyWage :Int = 0
-    var empTotalWage :Int = 0
-    var totalWorkHrs :Int = 0
-    var totalWorkingDays :Int = 0
+
+    let Is_Full_Time = 1
+    let Is_Part_Time = 2
+
+    init(){
+
+
+    }
+
+    func computeWage(empRate: Int, workingDays: Int, maxHrs: Int, company :String){
+
+        var empHrs = 0
+        var totalWorkHrs  = 0
+        var totalWorkingDays  = 0
+        var empTotalWage = 0
+        var empDailyWage = 0
+
+        while (totalWorkHrs <= 100 && totalWorkingDays < 20)
+        {
+            totalWorkingDays += 1
+        let empCheck = Int.random(in: 0..<5)
+        switch empCheck
+        {
+        case Is_Part_Time:
+            empHrs = 4
+
+
+        case Is_Full_Time:
+            empHrs = 8
+
+        default: empHrs = 0
+        }
+        totalWorkHrs += empHrs
+        empDailyWage = empRate * empHrs
+        empTotalWage += empDailyWage
+
+
+        }
+        print("Total wage",(empTotalWage),"of company",(company))
+
+    }
 
 }
 
-let Is_Full_Time = 1
-let Is_Part_Time = 2
-let Wage_Per_Hr = 20
-let Working_Days = 20
-let Work_Hours = 100
-
-var empHrs = 0
-let employee = EmployeeWage()
-while (employee.totalWorkHrs <= 100 && employee.totalWorkingDays < 20)
-{
-    employee.totalWorkingDays += 1
-let empCheck = Int.random(in: 0..<5)
-switch empCheck
-{
-case Is_Part_Time:
-    print("Part time employement")
-    empHrs = 4
-    
-
-case Is_Full_Time:
-    print("Full time employement")
-    empHrs = 8
-   
-default: empHrs = 0
-}
-    employee.totalWorkHrs += empHrs
-    employee.empDailyWage = Wage_Per_Hr * empHrs
-    employee.empTotalWage += employee.empDailyWage
- 
-    print("Daily wage" ,(employee.empDailyWage))
-
-}
-print("Total wage",(employee.empTotalWage))
-
-
+let company = EmployeeWage();
+company.computeWage(empRate: 20, workingDays: 10, maxHrs: 100, company: "Appple")
+company.computeWage(empRate: 25, workingDays: 10, maxHrs: 200, company: "Reliance")
